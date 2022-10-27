7cfc7f9caf9914f27ec145fac8b966298962125c
DiegoM <medeldiego47@yahoo.com>
2022-10-26 23:06:59 -0500

    finished get and post for categories routes

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -6,14 +6,50 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({
+    attributes:['id','category_name'],
+    include :[
+     { model: Product,
+      attributes:['id','product_name','price']
+  }]
+  }).then((categories)=> 
+  res.json(categories)).catch(err=>{
+    res.json(500).json(err);
+    throw (err);
+
+  })
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({
+    where:{
+      id:req.params.id
+    },
+    attributes:['category_name','id'],
+    include:[
+      {model:Product,
+      attributes:['id','product_name','price']}
+    ]
+  }).then(result=>{
+    if (!result){
+      res.status(404).json({message: `id does not match any category`});
+      return;
+    } else {res.json(result)};
+  }).catch((err)=> {
+    res.status(500).json(err);
+    throw(err)
+  })
 });
 
 router.post('/', (req, res) => {
+  Category.create({
+    category_name: req.body.category_name,
+  }).then(results=> 
+    res.json(results)).catch((err)=> {
+       res.status(500).json(err);
+      throw(err)})
   // create a new category
 });
 
